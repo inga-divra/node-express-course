79246f6337e2fd2a1d76bcd6327da9360f6bbf17
inga-divra <ingady@gmail.com>
2025-03-14 18:55:35 +0200

    Learned to use Express route parameters for handling dynamic routes and improved product retrieval

--- 02-express-tutorial/learned/07-params-query.js
+++ 02-express-tutorial/learned/07-params-query.js
@@ -14,6 +14,25 @@ app.get('/api/products', (req, res) => {
   res.json(newProducts);
 });
 
+app.get('/api/products/:productID', (req, res) => {
+  console.log(req);
+  console.log(req.params);
+
+  const { productID } = req.params; //returns a string{ ProductID: '1' }
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send('<h1>404 Product does not exist</h1>');
+  }
+  return res.json(singleProduct);
+});
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params);
+  res.send('Hello world!');
+});
+
 app.listen(5000, () => {
   console.log('Server is listening on port 5000...');
 });
