f0666d1605e496b6eff186c630bfbb6dc154dd74
inga-divra <ingady@gmail.com>
2025-03-16 18:03:17 +0200

    Learned middleware concept basics in Express
    Implemented a basic middleware function logger to log request details (method, URL, and timestamp). Middleware is now applied to the / and /about routes, demonstrating how it runs before sending a response. This helps in request tracking and debugging.

---
+++ 02-express-tutorial/learned/08-middleware-basic.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+//req => middleware => res
+
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
+
+app.get('/', logger, (req, res) => {
+  res.send('<h1>Home</h1>');
+});
+
+app.get('/about', logger, (req, res) => {
+  res.send('<h1>About</h1>');
+});
+
+app.listen(5000, () => {
+  console.log('Sever is listening on port 5000...');
+});
