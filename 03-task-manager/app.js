eb947016ca572c44929159142529186d62df9ac4
inga-divra <ingady@gmail.com>
2025-03-18 12:29:28 +0200

    Added routes to the Express app for task management

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+//routes
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App');
+});
+
+//app.get('/api/v1/tasks')               - GET all the tasks
+//app.post('/api/v1/tasks')              - CREATE a new task
+//app.get('/api/v1/tasks/:id')           - GET single task
+//app.patch('/api/v1/tasks/:id')         - UPDATE task
+//app.delete('/api/v1/tasks/:id')        - DELETE task
+
+const port = 3000;
+app.listen(port, console.log(`Server is listening on port ${port}...`));
