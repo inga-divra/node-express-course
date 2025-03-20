45c3212885687ee7d389f68d332f7be5971bfaf2
inga-divra <ingady@gmail.com>
2025-03-20 19:06:42 +0200

    installed dotenv and updated MongoDB connection to use environment variable

--- 03-task-manager/db/connect.js
+++ 03-task-manager/db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString =
-  'mongodb+srv://ingady:paesok1963@nodeexpressprojects.shkmp.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects';
-
 const connectDB = (url) => {
-  return mongoose.connect(connectionString, {});
+  return mongoose.connect(url, {});
 };
 
 module.exports = connectDB;
