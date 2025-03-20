d84cec59414c6900b6f032fa5ff4ebfa7a56c944
inga-divra <ingady@gmail.com>
2025-03-20 18:43:50 +0200

    Established MongoDB connection with async setup

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 
 //middleware
 
@@ -19,4 +20,14 @@ app.use('/api/v1/tasks', tasks);
 //app.delete('/api/v1/tasks/:id')        - DELETE task
 
 const port = 3000;
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- 03-task-manager/db/connect.js
+++ 03-task-manager/db/connect.js
@@ -1,2 +1,10 @@
-const connestionString =
-  'mongodb+srv://ingady:<db_password>@nodeexpressprojects.shkmp.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects';
+const mongoose = require('mongoose');
+
+const connectionString =
+  'mongodb+srv://ingady:paesok1963@nodeexpressprojects.shkmp.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects';
+
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {});
+};
+
+module.exports = connectDB;
