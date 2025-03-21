3b7db3a426aef327ab1c41a1b120983bf31e0680
inga-divra <ingady@gmail.com>
2025-03-21 11:22:44 +0200

    Implemented deleteTask controller using Mongoose findOneAndDelete

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -35,8 +35,19 @@ const updateTask = (req, res) => {
   res.send('Update task');
 };
 
-const deleteTask = (req, res) => {
-  res.send('Delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `Mo task with id: ${taskID}` });
+    }
+    //res.status(200).json({ task });
+    //res.status(200).send();
+    res.status(200).json({ task: null, status: 'success' });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
