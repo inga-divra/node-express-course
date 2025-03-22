286cbb791c4cbeb262cd052178efcc90c1b84163
inga-divra <ingady@gmail.com>
2025-03-22 19:40:06 +0200

    Set up routes for handling product requests

--- 04-store-api/app.js
+++ 04-store-api/app.js
@@ -1,32 +1,37 @@
 require('dotenv').config();
+// async errors
+
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
-// middleware
+//middleware
+
 app.use(express.json());
 
-// routes
+//routes
+
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="api/v1/products">Products route</a>');
 });
+
+app.use('/api/v1/products', productsRouter);
+
+//product route
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
-
 const start = async () => {
   try {
-    console.log('Mongo URI:', process.env.MONGO_URI); // Проверка загруженной строки
-    await connectDB(process.env.MONGO_URI); // Подключение через переменную окружения
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
