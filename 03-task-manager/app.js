332c3262240633ecd9cf445ec43460fce0858022
inga-divra <ingady@gmail.com>
2025-03-22 18:16:54 +0200

    Update app.js

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -22,7 +22,7 @@ app.use(notFound);
 //CUSTOM ERROR HANDLER
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
