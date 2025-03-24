88d36fba718ecf96925eaebf7c1e56b981284a33
inga-divra <ingady@gmail.com>
2025-03-24 11:47:51 +0200

    Learned how to implement dynamic product filtering based on query parameter.

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,10 +1,21 @@
-const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors');
+const Product = require('../models/product');
 
-  res.status(200).json({ msg: 'products testing route' });
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    featured: true,
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
