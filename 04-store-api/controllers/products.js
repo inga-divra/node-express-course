83514a96c8acfa11642cbf4418d817030a08e045
inga-divra <ingady@gmail.com>
2025-03-24 12:02:55 +0200

    Added company filter to the product search functionality
    Now products can be filtered by both 'featured' status and 'company'.
    If the 'company' query parameter is passed, it filters products by the specified company.

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -7,10 +7,13 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
   }
   console.log(queryObject);
 
