820e6ca88be96faba436dc4db2de274416d96fae
inga-divra <ingady@gmail.com>
2025-03-24 15:24:31 +0200

    Added product filtering by name with case-insensitive search

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,22 +1,25 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
+  const search = 'a';
   const products = await Product.find({
-    featured: true,
+    name: { $regex: search, $options: 'i' },
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
   if (company) {
     queryObject.company = company;
   }
-  console.log(queryObject);
-
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
   const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
