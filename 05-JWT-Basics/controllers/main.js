cce30318ecaccae55dbdea5e5fec42bdff7e5d58
inga-divra <ingady@gmail.com>
2025-03-25 19:19:00 +0200

    feat: added JWT signing and tested in Postman

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -3,6 +3,7 @@
 // send back to fron-end
 // setup authentication so only the request with JWT can access the dasboard
 
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
@@ -16,10 +17,21 @@ const login = async (req, res) => {
     throw new CustomAPIError('Please provide email and password', 400);
   }
 
-  res.send('Fake Login/Register/Signup Route');
+  //just for demo, normally provided by DB!
+  const id = new Date().getDate();
+
+  //try to keep payload small, better experience for user
+  // just for demo, in production use long,complex and unguessable string value!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
+  console.log(req.headers);
+
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John Doe`,
