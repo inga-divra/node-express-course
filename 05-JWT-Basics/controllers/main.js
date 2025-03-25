a9a0abd09255f06ae61925ab7eb449a4f192f822
inga-divra <ingady@gmail.com>
2025-03-25 18:50:40 +0200

    feat: added username and password validation in login request

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -1,11 +1,28 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to fron-end
+// setup authentication so only the request with JWT can access the dasboard
+
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  //mongoose validation
+  //Joi
+  //check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+
   res.send('Fake Login/Register/Signup Route');
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
-    msg: `Hello, JOhn Doe`,
+    msg: `Hello, John Doe`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
 };
