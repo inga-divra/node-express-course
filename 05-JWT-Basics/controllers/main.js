4b3d61e177b008481cb98c07d3be4f3281d29b9d
inga-divra <ingady@gmail.com>
2025-03-26 09:42:15 +0200

    refactor(errors): integrate custom error classes for authentication and validation
    Replaced generic errors with BadRequestError and UnauthenticatedError for improved error handling.
    
    Updated authenticationMiddleware to throw UnauthenticatedError for token validation failures.
    
    Modified login controller to throw BadRequestError for missing username or password.
    
    Updated errorHandlerMiddleware to properly handle CustomAPIError instances and return appropriate status codes.

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -4,7 +4,7 @@
 // setup authentication so only the request with JWT can access the dasboard
 
 const jwt = require('jsonwebtoken');
-const CustomAPIError = require('../errors/custom-error');
+const { BadRequestError } = require('../errors');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -14,7 +14,7 @@ const login = async (req, res) => {
   //check in the controller
 
   if (!username || !password) {
-    throw new CustomAPIError('Please provide email and password', 400);
+    throw new BadRequestError('Please provide email and password');
   }
 
   //just for demo, normally provided by DB!
@@ -30,8 +30,6 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  console.log(req.user);
-
   const luckyNumber = Math.floor(Math.random() * 100);
 
   res.status(200).json({
