b878c9275221ff6d29990cf68e0cde8d0a585be5
inga-divra <ingady@gmail.com>
2025-04-01 19:05:06 +0300

    chore: update project dependencies

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
