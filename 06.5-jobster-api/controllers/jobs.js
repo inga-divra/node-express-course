0bd44a831482b3d8eeb3634eb8126758c250cf36
inga-divra <ingady@gmail.com>
2025-04-09 18:45:35 +0300

    feat: set up basic stats route and controller

--- 06.5-jobster-api/controllers/jobs.js
+++ 06.5-jobster-api/controllers/jobs.js
@@ -116,10 +116,17 @@ const deleteJob = async (req, res) => {
   res.status(StatusCodes.OK).send();
 };
 
+const showStats = (req, res) => {
+  res
+    .status(StatusCodes.OK)
+    .json({ defaultStats: {}, monthlyApplications: [] });
+};
+
 module.exports = {
   createJob,
   deleteJob,
   getAllJobs,
   updateJob,
   getJob,
+  showStats,
 };
