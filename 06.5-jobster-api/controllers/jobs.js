3bf4f40af0a5a95b3743630b32817ce18c5715ca
inga-divra <ingady@gmail.com>
2025-04-10 18:37:27 +0300

    delete consolelog

--- 06.5-jobster-api/controllers/jobs.js
+++ 06.5-jobster-api/controllers/jobs.js
@@ -170,7 +170,6 @@ const showStats = async (req, res) => {
         return { date, count };
       })
       .reverse();
-    console.log(monthlyApplications);
 
     res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
   } catch (error) {
