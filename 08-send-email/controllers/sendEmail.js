b07e08a53a2054bb63c8516963650397c049f925
inga-divra <ingady@gmail.com>
2025-04-28 18:38:17 +0300

    feat(email): add nodemailer functionality and test email sending with Ethereal

--- 08-send-email/controllers/sendEmail.js
+++ 08-send-email/controllers/sendEmail.js
@@ -1,5 +1,29 @@
+const nodemailer = require('nodemailer');
+
 const sendEmail = async (req, res) => {
-  res.send('send email func');
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'emmy63@ethereal.email',
+      pass: '3qNut5QbfwPsYUeNGB',
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Coding Addict" <codingaddict@gmail.com>',
+    to: 'bar@example.com',
+    subject: 'Hello',
+    html: '<h2>Sending Emails with Node.js</h2>',
+  });
+
+  res.json(info);
 };
 
 module.exports = sendEmail;
