a5a004c7f868e1583f9e6c0190cbfbb0239fc6c6
inga-divra <ingady@gmail.com>
2025-04-30 19:04:28 +0300

    feat(payment): set up Stripe controller for creating payment intents

--- 09-stripe-payment/public/browser-app.js
+++ 09-stripe-payment/public/browser-app.js
@@ -6,7 +6,7 @@ const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+  'pk_test_51Qmuj9JZwtdg8EwYyC9WvyZjpu8JsB6Cw9e0jr6FFk56vUgIn49WpC69NEOFDWHIUl7jETQF4MnVQySMuFTM3sDp00PKMt31Uv'
 );
 
 // The items the customer wants to buy
