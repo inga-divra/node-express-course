8ab810f2f4c5335165bae6440d4cf2a9737c6081
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-05-05 19:15:55 +0300

    feat(models): add email validation with validator.js in User model
    - Used validator.isEmail for validating email field in UserSchema
    - Added custom error message for invalid email format

--- 10-e-commerce-api/models/User.js
+++ 10-e-commerce-api/models/User.js
@@ -1,5 +1,5 @@
-const { required } = require('joi');
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -11,6 +11,10 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide valid email',
+    },
   },
   password: {
     type: String,
