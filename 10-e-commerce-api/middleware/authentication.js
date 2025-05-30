a66e429fe246f4a26fe83a8459b4f293984f763f
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-05-30 18:36:31 +0300

    feat: implement token validation and user extraction in authenticateUser middleware

--- 10-e-commerce-api/middleware/authentication.js
+++ 10-e-commerce-api/middleware/authentication.js
@@ -1,15 +1,19 @@
-const CustomErr = require('../errors');
+const CustomError = require('../errors');
 const { isTokenValid } = require('../utils');
 
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
 
   if (!token) {
-    console.log('error, no token present');
-  } else {
-    console.log('token present');
+    throw new CustomError.UnauthenticatedError('Authentication invalid');
   }
-  next();
+  try {
+    const { name, userId, role } = isTokenValid({ token });
+    req.user = { name, userId, role };
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError('Authentication invalid');
+  }
 };
 
 module.exports = {
