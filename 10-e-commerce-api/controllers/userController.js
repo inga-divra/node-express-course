9398449017b62a8a6abf2c09842eb757a4803da8
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-05-30 18:08:54 +0300

    feat: add getSingleUser controller to fetch a user by ID excluding password

--- 10-e-commerce-api/controllers/userController.js
+++ 10-e-commerce-api/controllers/userController.js
@@ -8,7 +8,11 @@ const getAllUsers = async (req, res) => {
 };
 
 const getSingleUser = async (req, res) => {
-  res.send('get single user');
+  const user = await User.findOne({ _id: req.params.id }).select('-password');
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id: ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
