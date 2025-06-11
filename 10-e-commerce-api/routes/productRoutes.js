feba97ff6f1709ca997895d521fd4b471fa0a237
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-06-11 17:15:32 +0300

    feat: set up product routes with authentication and admin authorization

---
+++ 10-e-commerce-api/routes/productRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
+
+const {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  uploadProduct,
+} = require('../controllers/productController');
+
+router
+  .route('/')
+  .post([authenticateUser, authorizePermissions('admin')], createProduct)
+  .get(getAllProducts);
+
+router
+  .route('uploadImage')
+  .post([authenticateUser, authorizePermissions('admin')], updateProduct);
+
+router
+  .route('/:id')
+  .get(getSingleProduct)
+  .patch([authenticateUser, authorizePermissions('admin')], updateProduct)
+  .delete([authenticateUser, authorizePermissions('admin')], deleteProduct);
+
+module.exports = router;
