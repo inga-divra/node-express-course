832f2588a7bb5536e86f4e210f372637be743dbf
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-06-23 13:42:00 +0300

    feat: add deleteReview controller logic with permission check and removal

--- 10-e-commerce-api/controllers/reviewController.js
+++ 10-e-commerce-api/controllers/reviewController.js
@@ -53,7 +53,17 @@ const updateReview = async (req, res) => {
 };
 
 const deleteReview = async (req, res) => {
-  res.send('Delete review');
+  const { id: reviewId } = req.params;
+
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+  }
+
+  checkPermissions(req.user, review.user);
+  await review.remove();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Review removed' });
 };
 
 module.exports = {
