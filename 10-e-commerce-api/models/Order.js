7cef12476464d5c3d47638969ee3d32c2b034464
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-06-26 18:01:47 +0300

    feat(order): create Order model with fields for payment, cart items, and user reference

---
+++ 10-e-commerce-api/models/Order.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: [],
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
+      default: 'pending',
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentIntentId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Order', OrderSchema);
